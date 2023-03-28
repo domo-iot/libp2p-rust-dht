d7c2480ed183211fa9d21968af3bc1ebee4ca287
Domenico De Guglielmo <domenico.deguglielmo@domo-iot.com>
2023-03-28 15:05:35 +0200

    docker and github action

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# DoMO gateway target
 cross build --release --target aarch64-unknown-linux-musl --features=vendored
 
+# amd64/alpine
+cross build --release --target x86_64-unknown-linux-musl --features=vendored
 
