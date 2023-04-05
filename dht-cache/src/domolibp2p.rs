4557bd6435e8177d057dd3781df0e386b07559a4
Domenico De Guglielmo <domenico.deguglielmo@domo-iot.com>
2023-04-05 20:33:17 +0200

    explicit peer ticks

--- dht-cache/src/domolibp2p.rs
+++ dht-cache/src/domolibp2p.rs
@@ -104,7 +104,8 @@ pub async fn start(
         // Set a custom gossipsub
         let gossipsub_config = gossipsub::ConfigBuilder::default()
             .idle_timeout(Duration::from_secs(10))
-            .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+            .heartbeat_interval(Duration::from_secs(3)) // This is set to aid debugging by not cluttering the log space
+            .check_explicit_peers_ticks(10)
             .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
             .message_id_fn(message_id_fn) // content-address messages. No two messages of the
             // same content will be propagated.
