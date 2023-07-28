31bf87a9f08309cdf5557f93798a90ee8fcf9aef
Luca Barbato <lu_zero@gentoo.org>
2023-07-28 10:29:50 +0200

    Split the behavior creation from the swarm setup
    Should make easier testing the network component using
    libp2p_swarm_test.

--- dht-cache/src/domolibp2p.rs
+++ dht-cache/src/domolibp2p.rs
@@ -92,11 +92,6 @@ pub async fn start(
 ) -> Result<Swarm<DomoBehaviour>, Error> {
     let local_peer_id = PeerId::from(local_key_pair.public());
 
-    // Create a Gossipsub topic
-    let topic_persistent_data = Topic::new("domo-persistent-data");
-    let topic_volatile_data = Topic::new("domo-volatile-data");
-    let topic_config = Topic::new("domo-config");
-
     let arr = parse_hex_key(&shared_key)?;
     let psk = PreSharedKey::new(arr);
 
@@ -104,50 +99,7 @@ pub async fn start(
 
     // Create a swarm to manage peers and events.
     let mut swarm = {
-        let mdnsconf = mdns::Config {
-            ttl: Duration::from_secs(600),
-            query_interval: Duration::from_secs(30),
-            enable_ipv6: false,
-        };
-
-        let mdns = mdns::tokio::Behaviour::new(mdnsconf, local_peer_id)?;
-
-        // To content-address message, we can take the hash of message and use it as an ID.
-        let message_id_fn = |message: &gossipsub::Message| {
-            let mut s = DefaultHasher::new();
-            message.data.hash(&mut s);
-            MessageId::from(s.finish().to_string())
-        };
-
-        // Set a custom gossipsub
-        let gossipsub_config = gossipsub::ConfigBuilder::default()
-            .idle_timeout(Duration::from_secs(10))
-            .heartbeat_interval(Duration::from_secs(3)) // This is set to aid debugging by not cluttering the log space
-            .check_explicit_peers_ticks(10)
-            .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-            .message_id_fn(message_id_fn) // content-address messages. No two messages of the
-            // same content will be propagated.
-            .build()
-            .expect("Valid config");
-
-        // build a gossipsub network behaviour
-        let mut gossipsub = gossipsub::Behaviour::new(
-            MessageAuthenticity::Signed(local_key_pair),
-            gossipsub_config,
-        )
-        .expect("Correct configuration");
-
-        // subscribes to persistent data topic
-        gossipsub.subscribe(&topic_persistent_data).unwrap();
-
-        // subscribes to volatile data topic
-        gossipsub.subscribe(&topic_volatile_data).unwrap();
-
-        // subscribes to config topic
-        gossipsub.subscribe(&topic_config).unwrap();
-
-        let behaviour = DomoBehaviour { mdns, gossipsub };
-        //Swarm::new(transport, behaviour, local_peer_id)
+        let behaviour = DomoBehaviour::new(&local_key_pair)?;
 
         SwarmBuilder::with_tokio_executor(transport, behaviour, local_peer_id).build()
     };
@@ -169,6 +121,62 @@ pub async fn start(
 pub struct DomoBehaviour {
     pub mdns: libp2p::mdns::tokio::Behaviour,
     pub gossipsub: gossipsub::Behaviour,
+}
+
+impl DomoBehaviour {
+    pub fn new(local_key_pair: &crate::Keypair) -> Result<Self, Error> {
+        let local_peer_id = PeerId::from(local_key_pair.public());
+        let topic_persistent_data = Topic::new("domo-persistent-data");
+        let topic_volatile_data = Topic::new("domo-volatile-data");
+        let topic_config = Topic::new("domo-config");
+
+        let mdnsconf = mdns::Config {
+            ttl: Duration::from_secs(600),
+            query_interval: Duration::from_secs(30),
+            enable_ipv6: false,
+        };
+
+        let mdns = mdns::tokio::Behaviour::new(mdnsconf, local_peer_id)?;
+
+        // To content-address message, we can take the hash of message and use it as an ID.
+        let message_id_fn = |message: &gossipsub::Message| {
+            let mut s = DefaultHasher::new();
+            message.data.hash(&mut s);
+            MessageId::from(s.finish().to_string())
+        };
+
+        // Set a custom gossipsub
+        // SAFETY: hard-coded configuration
+        let gossipsub_config = gossipsub::ConfigBuilder::default()
+            .idle_timeout(Duration::from_secs(10))
+            .heartbeat_interval(Duration::from_secs(3)) // This is set to aid debugging by not cluttering the log space
+            .check_explicit_peers_ticks(10)
+            .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+            .message_id_fn(message_id_fn) // content-address messages. No two messages of the
+            // same content will be propagated.
+            .build()
+            .expect("Valid config");
+
+        // build a gossipsub network behaviour
+        let mut gossipsub = gossipsub::Behaviour::new(
+            MessageAuthenticity::Signed(local_key_pair.to_owned()),
+            gossipsub_config,
+        )
+        .expect("Correct configuration");
+
+        // subscribes to persistent data topic
+        gossipsub.subscribe(&topic_persistent_data).unwrap();
+
+        // subscribes to volatile data topic
+        gossipsub.subscribe(&topic_volatile_data).unwrap();
+
+        // subscribes to config topic
+        gossipsub.subscribe(&topic_config).unwrap();
+
+        let behaviour = DomoBehaviour { mdns, gossipsub };
+
+        Ok(behaviour)
+    }
 }
 
 #[allow(clippy::large_enum_variant)]
